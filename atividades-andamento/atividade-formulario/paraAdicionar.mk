b50c7d9ad08013058a8e6e6048a398f448cb0078
Vitor G <124639936+vigmsousa@users.noreply.github.com>
2023-03-17 20:59:58 -0300

    17-03-2023: Adição de um arquivo para edição do CSS

---
+++ atividades-andamento/atividade-formulario/paraAdicionar.mk
@@ -0,0 +1,40 @@
+para as checkboxes {
+	position: absolute;
+	margin: 30px 0;
+	width: 310px;
+	border-bottom: 2ps solid white;
+}
+
+para os labels do checkbox {
+	position: absolute;
+	top: 50%;
+	left: 5px;
+	transform: translateY(-50%);
+	color: white;
+	font-size: 1em;
+	pointer-events: none;
+}
+
+input {
+	width: 100%;
+	height: 50px;
+	background: transparent;
+	border: none;
+	outline: none;
+	font-size: 1em;
+	padding: 0 35px 0 5px;
+	color: white;
+}
+
+fieldset {
+	background: transparent;
+	border: 2px solid rgba(255,255,255,0.5);
+	border-radius: 20px;
+	backdrop-filter: blur(15px);
+}
+
+https://icons.getbootstrap.com/icons/music-note-beamed/
+https://icons.getbootstrap.com/icons/film/
+https://icons.getbootstrap.com/icons/book/
+
+|-> ícones para colocar nas legends
