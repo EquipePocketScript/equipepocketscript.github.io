ca1aecbb63f8bbd6546d9eb5b5c67662fd780792
vigmsousa <vitor.gabriel.martins06@aluno.ifce.edu.br>
2023-04-22 18:52:58 -0300

    22-04-2023: Alterações finais

--- exemplos-sites/sitedefotografias/opcao2/simplelightbox-master/src/jquery-plugin-wrap.js
+++
@@ -1,6 +0,0 @@
-(function ($, window, document, undefined) {
-    'use strict';
-    $.fn.simpleLightbox = function (options) {
-        return new SimpleLightbox(this.get(), options);
-    }
-})(jQuery, window, document);
